576d670cb72ca99d5870d6e4a2dce2e2a09ace02
Daudi Wampamba <me@daudi,dev>
2025-10-04 00:42:30 +0100

    Fix formatting

--- hosts/minecraft_server/mc-server.nix
+++ hosts/minecraft_server/mc-server.nix
@@ -10,24 +10,27 @@ let
     url = "https://github.com/Majonez57/MunchCraft/raw/0.0.5-alpha/pack.toml";
     packHash = "sha256-L5RiSktqtSQBDecVfGj1iDaXV+E90zrNEcf4jtsg+wk=";
   };
-  forge-installer = pkgs.fetchurl "https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.3.33/forge-1.20.1-47.3.33-installer.jar"
-in {
+  forge-installer = pkgs.fetchurl "https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.3.33/forge-1.20.1-47.3.33-installer.jar";
+in
+{
   services.minecraft-servers.munchcraft = {
     enable = true;
     eula = true;
-    package = pkgs.vanillaServers.vanilla-1_20_1.overrideAttrs (final: prev: {
-	preFixupPhase = prev.preFixupPhase + ''
-	    cat > $out/bin/minecraft-server << EOF
-            #!/bin/sh
-    	    exec ${jre_headless}/bin/java \$@ -jar $out/lib/minecraft/server.jar nogui
-            EOF
-	    
-	    java -Duser.dir=$out/lib -jar ${forge-installer} --installServer
-	'';
-    });
+    package = pkgs.vanillaServers.vanilla-1_20_1.overrideAttrs (
+      final: prev: {
+        preFixupPhase = prev.preFixupPhase + ''
+          	    cat > $out/bin/minecraft-server << EOF
+                      #!/bin/sh
+              	    exec ${jre_headless}/bin/java \$@ -jar $out/lib/minecraft/server.jar nogui
+                      EOF
+          	    
+          	    java -Duser.dir=$out/lib -jar ${forge-installer} --installServer
+          	'';
+      }
+    );
     declarative = true;
     symlinks = {
-    	"mods" = "${modpack}/mods";
+      "mods" = "${modpack}/mods";
     };
 
     openFirewall = true;
