e803623205fd552a931ba7d9731c14f65cc7fece
Daudi Wampamba <me@daudi,dev>
2025-10-04 20:30:00 +0100

    Upgrade Forge?

--- hosts/minecraft-server/mc-server.nix
+++ hosts/minecraft-server/mc-server.nix
@@ -7,12 +7,14 @@
   ...
 }:
 let
+  minecraft-verison = "1.20.1";
+  forge-version = "47.4.0";
   modpack = pkgs.fetchPackwizModpack {
     url = "https://github.com/Majonez57/MunchCraft/raw/0.0.5-alpha/pack.toml";
     packHash = "sha256-175qWQJaW0io4dkTgFar6Ih70EAY7xVkLghBKZmQueU=";
   };
   forge-installer = pkgs.fetchurl {
-    url = "https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.4.0/forge-1.20.1-47.4.0-installer.jar";
+    url = "https://maven.minecraftforge.net/net/minecraftforge/forge/${minecraft-version}-${forge-version}/forge-${minecraft-version}-${forge-version}-installer.jar";
     hash = "sha256-8/V0ZeLL3DKBk/d7p/DJTLZEBfMe1VZ1PZJ16L3Abiw=";
   };
 in
@@ -27,7 +29,7 @@ in
     servers = {
       munchcraft = {
         enable = true;
-        package = pkgs.vanillaServers.vanilla-1_20_1.overrideAttrs (
+        package = pkgs.vanillaServers."vanilla-${minecraft-version}".overrideAttrs (
           final: prev:
           let
             jre = pkgs.jdk17;
@@ -40,7 +42,7 @@ in
               	        #!/bin/sh
               		${jre}/bin/java -jar ${forge-installer} --installServer || true
               		echo "\$@" > user_jvm_args.txt
-              		exec ${jre}/bin/java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.33/unix_args.txt nogui
+              		exec ${jre}/bin/java @user_jvm_args.txt @libraries/net/minecraftforge/forge/${minecraft-version}-${forge-version}/unix_args.txt nogui
               		EOF
             '';
           }
