4b3eb5d885c90dcb4fe10b7e71a16965e3ee10b5
Daudi Wampamba <me@daudi,dev>
2025-10-04 01:02:18 +0100

    fix server path

--- hosts/minecraft-server/default.nix
+++ hosts/minecraft-server/default.nix
@@ -8,8 +8,8 @@
 {
   imports = [
     (modulesPath + "/virtualisation/proxmox-lxc.nix")
-    connectivity.nix
-    mc-server.nix
+    ./connectivity.nix
+    ./mc-server.nix
   ];
 
   nixpkgs.config.allowUnfree = true;
