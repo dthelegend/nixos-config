0a80b28ce4bf53459270b1f2b125b33394f8b13e
Daudi Wampamba <me@daudi,dev>
2025-10-04 02:33:06 +0100

    actually run the mc server :facepalm:

--- hosts/minecraft-server/mc-server.nix
+++ hosts/minecraft-server/mc-server.nix
@@ -18,35 +18,40 @@ in
 
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
 
-  services.minecraft-servers.servers.munchcraft = {
+  services.minecraft-servers = {
     enable = true;
     eula = true;
-    package = pkgs.vanillaServers.vanilla-1_20_1.overrideAttrs (
-      final: prev: {
-        preFixupPhase = prev.preFixupPhase + ''
-          cat > $out/bin/minecraft-server << EOF
-          #!/bin/sh
-          exec ${prev.packages.jre_headless}/bin/java \$@ -jar $out/lib/minecraft/server.jar nogui
-          EOF
+    servers = {
+      munchcraft = {
+        enable = true;
+        package = pkgs.vanillaServers.vanilla-1_20_1.overrideAttrs (
+          final: prev: {
+            preFixupPhase = prev.preFixupPhase + ''
+              cat > $out/bin/minecraft-server << EOF
+              #!/bin/sh
+              exec ${prev.packages.jre_headless}/bin/java \$@ -jar $out/lib/minecraft/server.jar nogui
+              EOF
 
-          java -Duser.dir=$out/lib -jar ${forge-installer} --installServer
-        '';
-      }
-    );
-    declarative = true;
-    symlinks = {
-      "mods" = "${modpack}/mods";
+              java -Duser.dir=$out/lib -jar ${forge-installer} --installServer
+            '';
+          }
+        );
+        declarative = true;
+        symlinks = {
+          "mods" = "${modpack}/mods";
+        };
+
+        openFirewall = true;
+        serverProperties = {
+          difficulty = 3;
+          gamemode = 1;
+          motd = "It's declarative as f**k!";
+          white-list = true;
+          allow-cheats = true;
+        };
+
+        jvmOpts = "-Xms2G -Xmx8G";
+      };
     };
-
-    openFirewall = true;
-    serverProperties = {
-      difficulty = 3;
-      gamemode = 1;
-      motd = "It's declarative as f**k!";
-      white-list = true;
-      allow-cheats = true;
-    };
-
-    jvmOpts = "-Xms2G -Xmx8G";
   };
 }
