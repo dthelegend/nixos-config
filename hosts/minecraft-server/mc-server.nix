5f47be6a24571affef5657843465e49f0f0c9ec8
Daudi Wampamba <me@daudi,dev>
2025-10-05 02:04:44 +0100

    Amendments

--- hosts/minecraft-server/mc-server.nix
+++ hosts/minecraft-server/mc-server.nix
@@ -9,7 +9,7 @@
 let
   minecraft-version = "1.20.1";
   forge-version = "47.4.0";
-  
+
   modpack = pkgs.fetchPackwizModpack {
     url = "https://github.com/Majonez57/MunchCraft/raw/0.0.7-alpha/pack.toml";
     packHash = "sha256-IKEbRLrwKNgZpwKQoOeeYXS1gkZB4/qX7QnM+aDSzjE=";
@@ -29,63 +29,67 @@ in
     enable = true;
     eula = true;
     servers = {
-      munchcraft = let
-        minecraft-version-fixed = (lib.strings.concatStringsSep "_" (lib.versions.splitVersion minecraft-version));
-      in {
-        enable = true;
-        package = pkgs.vanillaServers."vanilla-${minecraft-version-fixed}".overrideAttrs (
-          final: prev:
-          let
-            jre = pkgs.jdk17;
-          in
-          {
-            installPhase = prev.installPhase + ''
-                            mv $out/lib/minecraft/server.jar $out/lib/minecraft/server-${prev.version}.jar 
+      munchcraft =
+        let
+          minecraft-version-fixed = (
+            lib.strings.concatStringsSep "_" (lib.versions.splitVersion minecraft-version)
+          );
+        in
+        {
+          enable = true;
+          package = pkgs.vanillaServers."vanilla-${minecraft-version-fixed}".overrideAttrs (
+            final: prev:
+            let
+              jre = pkgs.jdk17;
+            in
+            {
+              installPhase = prev.installPhase + ''
+                              mv $out/lib/minecraft/server.jar $out/lib/minecraft/server-${prev.version}.jar 
 
-                            cat > $out/bin/minecraft-server << EOF
-              	        #!/bin/sh
-              		${jre}/bin/java -jar ${forge-installer} --installServer || true
-              		echo "\$@" > user_jvm_args.txt
-              		exec ${jre}/bin/java @user_jvm_args.txt @libraries/net/minecraftforge/forge/${minecraft-version}-${forge-version}/unix_args.txt nogui
-              		EOF
-            '';
-          }
-        );
+                              cat > $out/bin/minecraft-server << EOF
+                	        #!/bin/sh
+                		${jre}/bin/java -jar ${forge-installer} --installServer || true
+                		echo "\$@" > user_jvm_args.txt
+                		exec ${jre}/bin/java @user_jvm_args.txt @libraries/net/minecraftforge/forge/${minecraft-version}-${forge-version}/unix_args.txt nogui
+                		EOF
+              '';
+            }
+          );
 
-        symlinks = {
-          "mods" = "${modpack}/mods";
+          symlinks = {
+            "mods" = "${modpack}/mods";
+          };
+
+          openFirewall = true;
+          serverProperties = {
+            # Server Admin
+            motd = "It's declarative as f**k!";
+            white-list = false;
+            network-compression-threshold = 1400;
+            region-file-compression = "lz4";
+
+            # Microsoft Spyware
+            enforce-secure-profile = false;
+            online-mode = false;
+
+            # Gameplay
+            difficulty = 2;
+            gamemode = 0;
+            force-gamemode = true;
+            allow-cheats = false;
+            allow-flying = true;
+
+            # World
+            level-name = "munchcraft-prime";
+            level-seed = "munchingrn";
+          };
+
+          operators = {
+            DTheLegend = "602b1408-6464-453f-a79e-18372981700c";
+          };
+
+          jvmOpts = "-Xms2G -Xmx16G";
         };
-
-        openFirewall = true;
-        serverProperties = {
-	  # Server Admin
-          motd = "It's declarative as f**k!";
-          white-list = false;
-	  network-compression-threshold = 1400;
-	  region-file-compression = "lz4";
-
-	  # Microsoft Spyware
-	  enforce-secure-profile = false;
-	  online-mode = false;
-          
-	  # Gameplay
-	  difficulty = 2;
-          gamemode = 0;
-	  force-gamemode = true;
-          allow-cheats = false;
-	  allow-flying = true;
-
-	  # World
-	  level-name = "munchcraft-prime";
-	  level-seed = "munchingrn";
-        };
-
-	operators = {
-	  DTheLegend = "602b1408-6464-453f-a79e-18372981700c";
-	};
-
-        jvmOpts = "-Xms2G -Xmx16G";
-      };
     };
   };
 }
